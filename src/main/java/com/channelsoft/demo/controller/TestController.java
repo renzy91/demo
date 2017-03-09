66475a2e92ecc0bf6bc4f08af6f2ece13ac1477b
2804 <2804@DESKTOP-VG2EG1D>
2017-03-09 14:06:53 +0800

    jsonp

--- src/main/java/com/channelsoft/demo/controller/TestController.java
+++ src/main/java/com/channelsoft/demo/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.channelsoft.demo.controller;
 
+import org.codehaus.jackson.map.util.JSONPObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,6 +15,21 @@ public class TestController {
 	public String testGo(){
 		System.out.println("123");
 		return "12345";
+	}
+	
+	/**
+	 * jsonp接口
+	 * @Title: testJsonp
+	 * @param callback
+	 * @return
+	 * @throws
+	 */
+	@RequestMapping("/testJsonp")
+	@ResponseBody
+	public JSONPObject testJsonp(String callback){
+		System.out.println("testJsonp");
+		String value = "testJsonp";
+		return new JSONPObject(callback, value);
 	}
 
 }
